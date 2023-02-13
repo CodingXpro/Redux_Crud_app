10ef247062a90c64d1ec01eadac7693b0e1f6ad5
krishna sahu <krishnassm420012@gmail.com>
2023-02-13 15:29:57 +0530

    redux Project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 
 import './App.css';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addUser, deleteUser, updateUserName } from './features/Users';
 
 function App() {
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [newUserName, setNewUserName] = useState("");
   return (
-    <div className='App'></div>
+    <div className='App'>
+      {" "}
+      <div className='addUser'>
+        <input type="text" placeholder='Name...' onChange={(e) => setName(e.target.value)} />
+        <input type="text" placeholder='Username...' onChange={(e) => setUsername(e.target.value)} />
+        <button onClick={dispatch(addUser({ id: userList[userList.length - 1].id + 1, name, username }))}>Add User</button>
+      </div>
+      <div className='displayUsers'>
+        {userList.map((users) => {
+          return (
+            <div>
+              <h1>{users.name}</h1>
+              <h1>{users.username}</h1>
+              <input type="text" placeholder='Username...' />
+              <button onClick={dispatch(updateUserName({ id: users.id, username: newUserName }))}>Update Username</button>
+              <button onClick={dispatch(deleteUser({ id: users.id }))}>Delete Username</button>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+import usersReducer from './features/Users'
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer
+
+  }
+})
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
 
